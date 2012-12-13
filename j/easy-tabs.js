a5dc4bea2f201ffcc1d90d7c5831d41b68d3e78d
Chris Rockwell <chris@chrisrockwell.com>
2012-12-13 15:45:29 -0500

    fixing tabs v2

--- j/easy-tabs.js
+++ j/easy-tabs.js
@@ -9,46 +9,45 @@
 $(document).ready(function() {
   // start on the first tab
   var starting_tab = 1;
-	// For ease of use, starting_tab
-	// takes a more easily understood value than 0
-	// and just handles it
-	hide_content(starting_tab - 1);
-	// listen for mouse/keyboard
-	// on the tabs
-	$('.tab').each(function(index, el) {
-	  $(this).click(function(event) {
-		  hide_content(index);
-			// stop link action
-			return false;
-		});
-	});
+  // For ease of use, starting_tab
+  // takes a more easily understood value than 0
+  // and just handles it
+  hide_content(starting_tab - 1);
+  // listen for mouse/keyboard
+  // on the tabs
+  $('.tab').each(function(index, el) {
+    $(this).click(function(event) {
+      hide_content(index);
+      // stop link action
+      return false;
+    });
+  });
 })
-	
-	/**
+
+  /**
 	 * Hide all content except with_exception_of
 	 * @param int with_exception_of
 	 *   indicates which content block should be visible
 	 * 	 Remember: lists, arrays, whatever start at 0, not 1
 	 */
-	function hide_content(with_exception_of)
+  function hide_content(with_exception_of)
 	{
-		// we have content blocks and the
-		// corresponding tabs
-		$content_blocks = $('.content-dynamic');
-		$content_tabs = $('.tab');
-		// first remove active-content from any
-		$($content_blocks)
-			.removeClass('active-content')
-			.addClass('inactive-content');
-		// secondly, use with_exception_of to show the correct one
-		$($content_blocks[with_exception_of])
-		  .removeClass('inactive-content')
-			.addClass('active-content');
-		
-		// remove the active state from all tabs
-		$($content_tabs)
-		  .removeClass('active-tab');
-		// add the active state to the active tab
-		$($content_tabs[with_exception_of])
-		  .addClass('active-tab');
-	}
+    // we have content blocks and the
+    // corresponding tabs
+    $content_blocks = $('.content-dynamic');
+    $content_tabs = $('.tab');
+    // first remove active-content from any
+    $($content_blocks)
+      .removeClass('active-content')
+      .addClass('inactive-content');
+    // secondly, use with_exception_of to show the correct one
+    $($content_blocks[with_exception_of])
+      .removeClass('inactive-content')
+      .addClass('active-content');
+    // remove the active state from all tabs
+    $($content_tabs)
+      .removeClass('active-tab');
+    // add the active state to the active tab
+    $($content_tabs[with_exception_of])
+      .addClass('active-tab');
+  }
